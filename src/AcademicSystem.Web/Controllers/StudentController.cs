011b9869b006f2d39ea42355808ca73e92dc7d7b
d-luan <dluan1285@gmail.com>
2025-11-26 12:07:43 -0300

    feat(web): implement student registration endpoint with service integration

---
+++ src/AcademicSystem.Web/Controllers/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AcademicSystem.ApplicationCore.Interfaces;
+using AcademicSystem.ApplicationCore.Entities;
+using AcademicSystem.Web.DTOs;
+
+namespace AcademicSystem.Web.Controllers;
+
+[ApiController]
+[Route("api/students")]
+public class StudentController : ControllerBase
+{
+    private readonly IStudentService _studentService;
+
+    public StudentController(IStudentService studentService)
+    {
+        _studentService = studentService;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Register([FromBody] RegisterStudentRequest request)
+    {
+        var address = new Address(
+            request.Street,
+            request.City,
+            request.State,
+            request.ZipCode
+        );
+
+        var createdStudent = await _studentService.RegisterStudentAsync(
+            userId: Guid.NewGuid().ToString(),
+            registrationNumber: request.RegistrationNumber,
+            address: address
+        );
+
+        return CreatedAtAction(
+            nameof(Register),
+            new { id = createdStudent.Id },
+            new { id = createdStudent.Id, registrationNumber = createdStudent.RegistrationNumber }
+        );
+    }
+}
