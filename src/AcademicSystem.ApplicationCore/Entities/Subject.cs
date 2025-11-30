cabade63e7ed6fda28eec040adaa57ba0f73fa8f
d-luan <dluan1285@gmail.com>
2025-11-30 10:35:34 -0300

    feat(core): inplement Subject entity with TDD

---
+++ src/AcademicSystem.ApplicationCore/Entities/Subject.cs
@@ -0,0 +1,29 @@
+﻿using AcademicSystem.ApplicationCore.Exceptions;
+
+namespace AcademicSystem.ApplicationCore.Entities;
+
+public class Subject : BaseEntity
+{
+    public string Name { get; private set; } = null!;
+    public int CourseId { get; private set; }
+    public int Workload { get; private set; }
+
+    protected Subject() { }
+
+    public Subject(string name, int courseId, int workload)
+    {
+        if (courseId <= 0)
+        {
+            throw new DomainException("Invalid Course ID.");
+        }
+
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new DomainException("Name is required.");
+        }
+        
+        this.Name = name;
+        this.CourseId = courseId;
+        this.Workload = workload;
+    }
+}
---
+++ tests/AcademicSystem.UnitTests/Entities/SubjectTests.cs
@@ -0,0 +1,32 @@
+﻿using AcademicSystem.ApplicationCore.Entities;
+using AcademicSystem.ApplicationCore.Exceptions;
+using Xunit;
+
+namespace AcademicSystem.UnitTests.Entities;
+
+public class SubjectTests
+{
+    [Fact]
+    public void Should_ThrowException_When_CourseId_IsInvalid()
+    {
+        Assert.Throws<DomainException>(() =>
+            new Subject("Algorithms", 0, 60));
+    }
+
+    [Fact]
+    public void Should_ThrowException_When_NameIsValid()
+    {
+        Assert.Throws<DomainException>(() =>
+            new Subject("", 1, 60));
+    }
+
+    [Fact]
+    public void Should_CreateSubject_When_Valid()
+    {
+        var subject = new Subject("Database", 10, 80);
+
+        Assert.NotNull(subject);
+        Assert.Equal("Database", subject.Name);
+        Assert.Equal(10, subject.CourseId);
+    }
+}
